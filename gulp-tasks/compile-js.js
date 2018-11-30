9c730b1c8ce6d081a3291353c3247e8712b454dc
Daniel Lewis <daniel_lewis@esri.com>
2018-11-29 20:29:54 -0800

    added watch to js

--- gulp-tasks/compile-js.js
+++ gulp-tasks/compile-js.js
@@ -4,21 +4,30 @@ const path = require("path");
 const jsCompiler = require('../compilers/js-compiler');
 
 const config = require('../config');
+const jsGlob = `${config.src_path}/${config.js_glob}`;
 
 gulp.task('compile-js', () => {
+    buildJsFiles();
+});
+
+gulp.task('watch-js', () => {
+    console.log('Watching Javascript Files');
+    gulp.watch(jsGlob, () => {
+        buildJsFiles();
+    });
+});
+
+function buildJsFiles() {
     console.log('Beginning Javascript Compile');
-    glob(`${config.src_path}/${config.js_glob}`, (err, jsFiles) => {
+    glob(jsGlob, (err, jsFiles) => {
         if (!err) {
             jsFiles.forEach((sourceJs) => {
                 const outputName = path.basename(sourceJs);
                 const outputDirName = path.dirname(sourceJs).replace(config.src_path, config.build_path);
-                const saveAs = `${outputDirName}/${outputName}`;
 
-                console.log(`compiling ${sourceJs} to ${saveAs}`);
+                console.log(`compiling ${sourceJs} to ${outputDirName}/${outputName}`);
                 jsCompiler({sourceJs, outputDirName, outputName});
             });
         }
     });
-});
-
-
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 
 require('require-dir')('./gulp-tasks');
 
-gulp.task('default', ["clean-dist","compile-js"], () => {});
-gulp.task('build', ["clean-dist", "compile-js"], () => {});
+gulp.task('default', ["clean-dist", "compile-js", "watch-js"], () => { });
+gulp.task('build', ["clean-dist", "compile-js"], () => { });
