64553ee1b495aae2c57a7041d08235b7a9256ead
Daniel Lewis <daniel_lewis@esri.com>
2018-12-01 08:36:19 -0800

    moved config file to .just-build-it

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
-const pkg = require.main.require(`${process.cwd()}/package.json`);
-const config = pkg.just_build_it;
+const fs = require('fs-extra');
+
+const configPath = `${process.cwd()}/.just-build-it`;
+const configFile = fs.readFileSync(configPath, 'utf-8');
+
+console.log('Initializing Just-Build-It with the following config:');
+console.log(configFile);
+
+const config = Object.assign({}, JSON.parse(configFile), {
+    "src_path": "./src",
+    "js_glob": "**/*.js",
+    "scss_glob": "**/*.scss",
+    "build_path": "./dist"
+});
 module.exports = config;
