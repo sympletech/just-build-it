10b12671a8c36cd91546999be0adea481743f7a4
Daniel Lewis <daniel_lewis@esri.com>
2018-12-01 08:44:58 -0800

    added webserver

--- config.js
+++ config.js
@@ -3,13 +3,16 @@ const fs = require('fs-extra');
 const configPath = `${process.cwd()}/.just-build-it`;
 const configFile = fs.readFileSync(configPath, 'utf-8');
 
-console.log('Initializing Just-Build-It with the following config:');
-console.log(configFile);
-
-const config = Object.assign({}, JSON.parse(configFile), {
+const config = Object.assign({}, {
     "src_path": "./src",
     "js_glob": "**/*.js",
     "scss_glob": "**/*.scss",
-    "build_path": "./dist"
-});
+    "build_path": "./dist",
+    "run_server": true,
+    "server_port": 3000
+}, JSON.parse(configFile));
+
+console.log('Initializing Just-Build-It with the following config:');
+console.log(config);
+
 module.exports = config;
---
+++ gulp-tasks/web-server.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+const config = require('../config');
+
+gulp.task('web-server', () => {
+    if (config.run_server) {
+        console.log('Starting Web Server');
+        browserSync.init({
+            open: false,
+            port: config.server_port,
+            server: {
+                baseDir: config.build_path,
+                directory: true
+            },
+            watchOptions: {
+                ignoreInitial: true
+            },
+            files: [
+                `${config.build_path}/**/*`
+            ],
+            cors: true,
+            reloadOnRestart: true
+        });
+    }
+});
