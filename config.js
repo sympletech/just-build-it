f36080b7bf7241672a524a8d5ebc08269c586857
Daniel Lewis <daniel_lewis@esri.com>
2018-12-03 16:14:20 -0800

    put config back

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const fs = require('fs-extra');
 
-// const configPath = `${process.cwd()}/.just-build-it`;
-// const configFile = fs.readFileSync(configPath, 'utf-8');
-const configFile = "{}";
+const configPath = `${process.cwd()}/.just-build-it`;
+const configFile = fs.readFileSync(configPath, 'utf-8');
 
 const config = Object.assign({}, {
     "src_path": "./src",
