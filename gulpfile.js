d4f385441946688d8b72350cf5a8eeeb23f2c8d8
Daniel Lewis <daniel_lewis@esri.com>
2018-12-04 16:38:32 -0800

    added watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,4 @@ require('require-dir')('./gulp-tasks');
 
 gulp.task('default', ["clean-dist", "copy-static-files", "watch-static-files", "compile-js", "watch-js", "compile-scss", "watch-scss", "web-server"], () => { });
 gulp.task('build', ["clean-dist", "copy-static-files", "compile-js", "compile-scss"], () => { });
+gulp.task('watch', ["watch-static-files", "watch-js", "watch-scss", "web-server"], () => { });
