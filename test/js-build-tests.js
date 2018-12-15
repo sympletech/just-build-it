d85e94c1f53f3982bc452e2065432a93c63ee1a1
Daniel Lewis <daniel_lewis@esri.com>
2018-12-14 20:21:30 -0800

    first unit test is passing

---
+++ test/js-build-tests.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const path = require('path');
+const fs = require('fs');
+
+const {promisify} = require('util');
+const stat = promisify(fs.stat);
+const unlink = promisify(fs.unlink);
+
+const jsCompiler = require('../compilers/js-compiler');
+
+it('should-build-a-basic-js-file', async () => {
+    try {
+        const workingDir = `${path.resolve(__dirname, '../test_src/js/should-build-a-basic-js-file')}`;
+        await jsCompiler({
+            sourceJs: `${workingDir}/test.js`,
+            outputDirName: `${workingDir}`,
+            outputName: 'build.js',
+            minify: false
+        });
+
+        const fileStats = await stat(`${workingDir}/build.js`);
+        expect(fileStats).to.not.be.null;
+
+        await unlink(`${workingDir}/build.js`);
+    } catch (err) {
+        expect(err).to.be(undefined);
+    }
+});
--- test/tests.js
+++
@@ -1,1 +0,0 @@
-// require('./js/js-build-tests')();
