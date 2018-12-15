6eb79560d3265a8b7d142faef756ab49536f67da
Daniel Lewis <daniel_lewis@esri.com>
2018-12-14 20:33:30 -0800

    added basic scss test

--- compilers/scss-compiler.js
+++ compilers/scss-compiler.js
@@ -4,19 +4,25 @@ const sassGlob = require('gulp-sass-glob');
 const sourcemaps = require('gulp-sourcemaps');
 
 function scssCompiler({sourceFolder, sourceFile, destFolder}) {
-	gulp.src(`${sourceFolder}/${sourceFile}`, {base: sourceFolder})
-		.pipe(sourcemaps.init())
-		.pipe(sourcemaps.identityMap())
-		.pipe(sassGlob())
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(destFolder))
-		.on('error', (err) => {
-			console.log(err);
-			this.emit('end');
-		});
+	return new Promise((resolve, reject) => {
+		gulp.src(`${sourceFolder}/${sourceFile}`, {base: sourceFolder})
+			.pipe(sourcemaps.init())
+			.pipe(sourcemaps.identityMap())
+			.pipe(sassGlob())
+			.pipe(sass({
+				outputStyle: 'compressed'
+			}))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest(destFolder))
+			.on('end', () => {
+				resolve();
+			})
+			.on('error', (err) => {
+				console.log(err);
+				this.emit('end');
+				reject(err);
+			});
+	});
 }
 
 module.exports = scssCompiler;
---
+++ test/scss-build-tests.js
@@ -0,0 +1,29 @@
+const expect = require('chai').expect;
+const path = require('path');
+const fs = require('fs');
+
+const {promisify} = require('util');
+const stat = promisify(fs.stat);
+const unlink = promisify(fs.unlink);
+
+const scssCompiler = require('../compilers/scss-compiler');
+
+it('should-build-a-basic-scss-file', async () => {
+    try {
+        const workingDir = `${path.resolve(__dirname, '../test_src/scss/should-build-a-basic-scss-file')}`;
+        await scssCompiler(
+            {
+                sourceFolder: `${workingDir}`,
+                sourceFile: `test.scss`,
+                destFolder: `${workingDir}`
+            });
+
+        const fileStats = await stat(`${workingDir}/test.css`);
+        expect(fileStats).to.not.be.null;
+
+        await unlink(`${workingDir}/test.css`);
+        await unlink(`${workingDir}/test.css.map`);
+    } catch (err) {
+        expect(err).to.be(undefined);
+    }
+});
