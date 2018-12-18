5470ceaf4372565ee8b6d862a79d4b2ab0c9dc85
Daniel Lewis <daniel_lewis@esri.com>
2018-12-17 19:31:13 -0800

    moveRemove-should-move-!-to-start

--- test/utils/utils-tests.js
+++ test/utils/utils-tests.js
@@ -2,6 +2,11 @@ const expect = require('chai').expect;
 const utils = require('../../utils/utils');
 
 describe('utils', () => {
+    it('moveRemove-should-move-!-to-start', () => {
+        const globDef = utils.moveRemove('./somepath/', '!**/*.txt');
+        expect(globDef).to.equal('!./somepath/**/*.txt');
+    });
+
     it('toGlobArray-should-return-single-glob', () => {
         const globArray = utils.toGlobArray({glob_def: 'test.scss', src_path: 'my/path'});
         expect(globArray).to.equal('my/path/test.scss');
