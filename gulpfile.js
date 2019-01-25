1fb3da0e123f1d373f4619a95961a01f6e43e47c
Daniel Lewis <daniel_lewis@esri.com>
2019-01-24 18:41:35 -0800

    gulp.series

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ const gulp = require('gulp');
 
 require('require-dir')('./gulp-tasks');
 
-gulp.task('default', ["clean-dist", "copy-static-files", "watch-static-files", "build-js-dev", "watch-js", "build-scss", "watch-scss", "web-server"], () => { });
-gulp.task('build', ["clean-dist", "copy-static-files", "build-js", "build-scss"], () => { });
-gulp.task('watch', ["watch-static-files", "watch-js", "watch-scss", "web-server"], () => { });
+gulp.task('default', gulp.series("clean-dist", "copy-static-files", "watch-static-files", "build-js-dev", "watch-js", "build-scss", "watch-scss", "web-server"));
+gulp.task('build', gulp.series("clean-dist", "copy-static-files", "build-js", "build-scss"));
+gulp.task('watch', gulp.series("watch-static-files", "watch-js", "watch-scss", "web-server"));
