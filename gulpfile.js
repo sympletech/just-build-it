5f8c5106d93f5b43a1cce6391eba1e7f22d33c7a
Daniel Lewis <daniel_lewis@esri.com>
2019-01-24 18:55:27 -0800

    gulp.parallel

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ const gulp = require('gulp');
 
 require('require-dir')('./gulp-tasks');
 
-gulp.task('default', gulp.series("clean-dist", "copy-static-files", "watch-static-files", "build-js-dev", "watch-js", "build-scss", "watch-scss", "web-server"));
-gulp.task('build', gulp.series("clean-dist", "copy-static-files", "build-js", "build-scss"));
-gulp.task('watch', gulp.series("watch-static-files", "watch-js", "watch-scss", "web-server"));
+gulp.task('default', gulp.parallel("clean-dist", "copy-static-files", "watch-static-files", "build-js-dev", "watch-js", "build-scss", "watch-scss", "web-server"));
+gulp.task('build', gulp.parallel("clean-dist", "copy-static-files", "build-js", "build-scss"));
+gulp.task('watch', gulp.parallel("watch-static-files", "watch-js", "watch-scss", "web-server"));
